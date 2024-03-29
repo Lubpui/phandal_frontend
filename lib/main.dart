28dbcc929c00ca75f4dbcdb0be78255c2cb47bf0
HamHarry <hamza_2641@hotmail.com>
2024-03-29 21:15:55 +0700

    SurryMan

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'GGEZ',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
